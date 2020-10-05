53e0eb2b8bc4e213b17355636068cafdcf2f3c3c
unknowableshade <60275123+unknowableshade@users.noreply.github.com>
2020-10-05 19:35:37 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,13 +1,35 @@
 import os
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!")
+bot = commands.Bot(command_prefix = '!') #bot commands prefix
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 @bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user.name}({bot.user.id})")
+async def on_ready(): #activates when bot is ready to work
+    print(f'--------------------------------')
+    print(f'wndrx is ready') 
+    print(f'user name: {bot.user.name}')
+    print(f'user id: {bot.user.id}')
+    print(f'--------------------------------')
 
+    game = discord.Game('kill mankind')
+    await bot.change_presence(status = discord.Status.idle, activity = game)
+
+
+@bot.command() #loads cogs
+async def load_ext(ctx, ext):
+    bot.load_extension(f'cogs.{ext}')
+
+
+@bot.command() #unload cogs
+async def unload_ext(ctx, ext):
+    bot.unload_extension(f'cogs.{ext}')
+
+
+for filename in os.listdir('./cogs'): #auto activate all cogs from /cogs directory
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+        
 @bot.command()
 async def ping(ctx):
     await ctx.send("pong")
