50b37fcb1a2973bb05472ad4a743ce68b2c9f691
Vladimir <deytenit@gmail.com>
2020-11-19 22:28:14 +0300

    alex-afonin bot added

--- bot/main.py
+++ bot/main.py
@@ -2,6 +2,29 @@ import os
 import discord
 from discord.ext import commands
 import json
+import telebot
+
+TOKEN1 = os.getenv("TELEBOT")
+bot1 = telebot.TeleBot(TOKEN1)
+cnt = 2
+
+@bot1.message_handler(commands=["start"])
+def start_chat(message):
+    if (message.from_user.username == "alexafonin123"):
+        bot1.send_message(message.chat.id, "@gordeve Кто?")
+    if (message.from_user.username == "YuriFilatov"):
+        bot1.send_message(message.chat.id, "Юра помолчи...", reply_to_message_id=message.message_id)
+
+
+@bot1.message_handler(content_types=["text"])
+def continue_chat(message):
+    global cnt
+    #print(str(message.from_user.username) + ' ' + str(message.from_user.first_name) + ' ' + str(message.from_user.last_name) + ': ' + str(message.text))
+    if (message.from_user.username == "gordeve"):
+        bot1.send_message(message.chat.id, "Неправильно, попробуй еще раз. Попытка: " + str(cnt), reply_to_message_id=message.message_id)
+        cnt += 1
+
+bot1.polling(none_stop = True)
 
 bot = commands.Bot(command_prefix = '!') #bot commands prefix
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -19,7 +42,7 @@ async def on_ready(): #activates when bot is ready to work
     game = discord.Game("github.com/unknowableshade/5a0a-bot")
     await bot.change_presence(status = discord.Status.idle, activity = game)
 
-
+ 
 @bot.command() #loads cogs
 async def load_ext(ctx, ext):
     bot.load_extension(f'cogs.{ext}')
