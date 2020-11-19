336483f04784e55fc771034126996c423c60c3ee
Vladimir <deytenit@gmail.com>
2020-11-19 23:11:57 +0300

    Still fixing

--- bot/abot.py
+++ bot/abot.py
@@ -7,10 +7,10 @@ cnt = 2
 
 @bot.message_handler(commands=["start"])
 def start_chat(message):
-    bot.send_message(message.chat.id, "@gordeve Кто?")
     if (message.from_user.username == "YuriFilatov"):
         bot.send_message(message.chat.id, "Юра помолчи...", reply_to_message_id=message.message_id)
-
+    else:
+        bot.send_message(message.chat.id, "@gordeve Кто?")
 
 @bot.message_handler(content_types=["text"])
 def continue_chat(message):
