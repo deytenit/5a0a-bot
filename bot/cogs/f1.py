d30f00b91d0c5bbbf1d1de96f6aa42b6e5aed410
unknowableshade <60275123+unknowableshade@users.noreply.github.com>
2021-07-02 17:25:47 +0300

    Update f1.py

--- bot/cogs/f1.py
+++ bot/cogs/f1.py
@@ -6,6 +6,7 @@ import os
 import urllib
 import random
 from urllib.request import urlopen
+import datetime
 
 path = os.path.dirname(__file__) + '/data'
 
@@ -39,6 +40,51 @@ class f1(commands.Cog):
             await ctx.send(standings)
         except urllib.error.HTTPError:
             await ctx.send('Server is down.')
+
+    @commands.command()
+    async def f1_schedule(self, ctx):
+        try:
+            schedule = json.loads(urlopen(f'http://ergast.com/api/f1/current.json').read())
+            schedule = schedule['MRData']['RaceTable']['Races']
+            season = schedule[0]['season']
+            table = f'**FIA Formula 1 {season} Schedule.**\n'
+            table += '```haskel\n' + 'round | name | date | time | circuit\n' + '____________________________________\n'
+
+            currentdate = datetime.date.today().strftime('%Y-%m-%d').split('-')
+
+            flag = False
+
+            i = 0
+
+            for race in schedule:
+                round = race['round']
+                name = race['raceName']
+                date = race['date']
+                tmplist = date.split('-')
+                time = race['time']
+                print(tmplist)
+                print(currentdate)
+                circuit = race['Circuit']['circuitName']
+                if (int(tmplist[0]) < int(currentdate[0])) or (int(tmplist[1]) < int(currentdate[1])) or (int(tmplist[2]) < int(currentdate[2])) or flag:
+                    if flag:
+                        table += f'{round} | {name} | {date} | {time} | {circuit}\n'
+                        i += 1
+                        if i == 8:
+                            break
+                else:
+                    flag = True
+                    table += f'| {round} | {name} | {date} | {time} | {circuit}\n____________________________________(current)\n'
+                
+            table += '____________________________________```\n'
+            
+            print(table)
+            await ctx.send(table)
+
+        except urllib.error.HTTPError:
+            await ctx.send('Server is down.')
+
+
+
 
 def setup(bot):
     bot.add_cog(f1(bot))
