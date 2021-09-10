7bd97535a2d378462ee7f9ff6297d2d218dc4a67
unknowableshade <60275123+unknowableshade@users.noreply.github.com>
2021-09-10 22:34:51 +0300

    Update music.py

--- bot/cogs/music.py
+++ bot/cogs/music.py
@@ -1,4 +1,5 @@
 #5a0a bot - Music cog
+import re
 import discord
 from discord.ext import commands
 from youtube_dl import YoutubeDL
@@ -6,234 +7,111 @@ import requests
 import asyncio
 import json
 import os
+import sqlite3
 
 PATH = os.path.dirname(__file__) + '/data'
 YDL_OPTIONS = {'format': 'bestaudio', 'playlistend': 50}
 FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 
+class Player:
+    def __init__(self, ctx):
+        self.bot = ctx.bot
+        self.guild = ctx.guild
+        self.channel = ctx.message.channel
+
+        self.info_msg = None
+        self.looping = 0
+        self.playing = False
+        self.queue_pos = 0
+        self.queue = []
+
+    async def play_music(self):
+        if len(self.queue) != 0:
+            self.playing = True
+
+            if self.looping == 0 and self.queue_pos == len(self.queue):
+                self.queue_pos = 0
+                self.playing = False
+                embed = discord.Embed(title='End of queue.')
+                self.info_msg = await self.channel.send(embed=embed)
+                return
+            elif self.looping == 1:
+                self.queue_pos = (self.queue_pos + 1) % len(self.queue)
+
+            song = self.queue[self.queue_pos]
+            self.guild.voice_client.play(discord.FFmpegPCMAudio(song['url'], **FFMPEG_OPTIONS), after=lambda e: self.play_music())
+
+            await self.info_msg.delete()
+            embed = discord.Embed(title=song['title'], url=song['url'], description='is now beeing played by' + song['requester'])
+            self.info_msg = await self.channel.send(embed=embed)
+
+
+
 class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+        self.players = []
+
+    def get_player(self, ctx):
+        if ctx.guild.id not in self.players:
+            self.players[ctx.guild.id] = Player(ctx)
         
-        self.is_looped = False
-        self.is_playing = False
-        self.queue_position = 0
-        self.music_queue = []
+        return self.players[ctx.guild.id]
 
-        self.info_msg = None
-        self.vc = None
-
-    def add_music(self, query):
+    def get_song(self, ctx, query):
+        response = []
         with YoutubeDL(YDL_OPTIONS) as ydl:
             try:
                 requests.get(query)
             except:
                 info = ydl.extract_info(f'ytsearch:{query}', download=False)['entries'][0]
-                self.music_queue.append({'title': info['title'], 'url': info['formats'][0]['url']})
+                response.append({'title': info['title'], 'url': info['formats'][0]['url'], 'requester': ctx.author})
             else:
                 info = ydl.extract_info(query, download=False)
                 if 'entries' in info:
-                    for entry in info['entries']:
-                        self.music_queue.append({'title': entry['title'], 'url': entry['formats'][0]['url']})
+                    for i, entry in enumerate(info['entries']):
+                        if i == 50:
+                            break
+                        response.append({'title': entry['title'], 'url': entry['formats'][0]['url'], 'requester': ctx.author})
                 else:
-                    self.music_queue.append({'title': info['title'], 'url': info['formats'][0]['url']})
+                    response.append({'title': info['title'], 'url': info['formats'][0]['url'], 'requester': ctx.author})
+            
+            return response
 
-    def play_music(self):
-        if len(self.music_queue) > 0:
-            self.is_playing = True
-            print(self.queue_position)
-
-            if self.queue_position >= len(self.music_queue):
-                if self.is_looped:
-                    self.queue_position = 0
-                else:
-                    self.queue_position = 0
-                    self.is_playing = False
-                    return
-
-            song = self.music_queue[self.queue_position]
-            print(song['url'])
-            self.queue_position += 1
-
-            self.vc.play(discord.FFmpegPCMAudio(song['url'], **FFMPEG_OPTIONS), after=lambda e: self.play_music())
-            return song['title']
-        else:
-            self.is_playing = False
-            return 'Queue is empty.'
-
-    def generate_queue(self, page):
-        post = '```ml\n'
-        for i in range(page * 10, min(page * 11 + 10, len(self.music_queue))):
-            if i + 1 == self.queue_position:
-                post += '    ⬐ current track\n' + str(i + 1) + ') ' + self.music_queue[i]['title'] + '\n' + '    ⬑ current track\n'
-            else:
-                post += str(i + 1) + ') ' + self.music_queue[i]['title'] + '\n'
-
-        if page * 11 + 10 >= len(self.music_queue):
-            post += '\nThis is the end of queue!\n'
-        else:
-            post += f'\nThere {len(self.music_queue) - (page * 11 + 10)} songs left.'
-
-        post += '```'
-        return post
-
-
-    @commands.Cog.listener()
-    async def on_voice_state_update(self, member, before, after):
-        if self.vc is None:
-            return 
-
-        if len(self.vc.channel.members) == 1:
-            self.vc.stop()
-            await self.vc.disconnect()
-            self.vc = None
-            self.is_playing = False
-        
     @commands.command()
-    async def connect(self, ctx):
+    async def connect_(self, ctx):
         channel = ctx.author.voice.channel
 
-        if self.vc is not None:
-            if self.vc.channel.id == channel.id:
+        vc = ctx.voice_client
+
+        if vc is not None:
+            if vc.channel.id == channel.id:
                 return
-            else:
-                await self.vc.move_to(channel)
+            elif vc.channel.id != channel.id:
+                await vc.move_to(channel)
         else:
-            self.vc = await channel.connect()
+            await channel.connect() 
 
-        await ctx.send('Connected.')
-
-
-    @commands.command(aliases=['p'])
-    async def play(self, ctx, *args):
-        query = ' '.join(args)
-        if len(args) != 0:
-            self.add_music(query)
-
-        if self.vc == None:
-            await ctx.invoke(self.connect)
-
-        if not self.is_playing:
-            await ctx.send(f'Now playing: **{self.play_music()}**')
-
-    @commands.command(aliases=['s'])
-    async def stop(self, ctx):
-        if self.is_playing:
-            self.vc.stop()
-            await self.vc.disconnect()
-            self.vc = None
-            self.is_playing = False
-            await ctx.send('Goodbye.')
-
-    @commands.command(aliases=['n', 'skip'])
-    async def next(self, ctx):
-        if self.is_playing:
-            self.vc.stop()
-
-        await ctx.invoke(self.play)
-
-    @commands.command(aliases=['q'])
-    async def queue(self, ctx):
-        if len(self.music_queue) > 0:
-            page = self.queue_position // 11
-            maxPage = len(self.music_queue) // 11 + 1
-               
-            msg = await ctx.send(self.generate_queue(page))
-            reactions = ['⏫', '🔼', '🔽', '⏬']
-            for emoji in reactions:
-                await msg.add_reaction(emoji)
-
-            flag = True
-
-            while flag:
-                try:
-                    reaction, user = await self.bot.wait_for('reaction_add', timeout=60, check=lambda reaction, user: str(reaction.emoji) in reactions and user.id != self.bot.user.id and msg == reaction.message)
-                    if reaction.emoji == reactions[0]:
-                        page = 0
-                    elif reaction.emoji == reactions[1] and page != 0:
-                        page -= 1
-                    elif reaction.emoji == reactions[2] and page != maxPage - 1:
-                        page += 1
-                    elif reaction.emoji == reactions[3]:
-                        page = maxPage - 1
-                    await reaction.remove(user)
-                    print(page)
-                    print(maxPage)
-                    await msg.edit(content=self.generate_queue(page))
-                except asyncio.TimeoutError:
-                    flag = False
-
-        else:
-            await ctx.send('Queue is empty.')
-
-    @commands.command(aliases=['l'])
-    async def loop(self, ctx):
-        self.is_looped = not self.is_looped
-        if self.is_looped:
-            await ctx.send('Now looping queue.')
-        else:
-            await ctx.send('Stoped looping queue.')
-
-    @commands.command(aliases=['r'])
-    async def remove(self, ctx, query):
-        if query.isnumeric() and int(query) <= len(self.music_queue):
-            del self.music_queue[int(query) - 1]
-            await ctx.invoke(self.next)
-        else:
-            for i in range(len(self.music_queue)):
-                if query in self.music_queue[i]['title']:
-                    del self.music_queue[i]
-                    await ctx.invoke(self.next)
-                    break
-
-    @commands.command(aliases=['j'])
-    async def jump(self, ctx, query):
-        if query.isnumeric() and int(query) <= len(self.music_queue):
-            self.queue_position = int(query) - 1
-            await ctx.invoke(self.next)
-        else:
-            for i in range(len(self.music_queue)):
-                if query in self.music_queue[i]['title']:
-                    self.queue_position = i
-                    await ctx.invoke(self.next)
-                    break
-
-    @commands.command(aliases=['c'])
-    async def clear(self, ctx):
-        if len(self.music_queue) != 0:
-            self.music_queue.clear()
-            self.queue_position = 0
-            self.vc.stop()
-            await ctx.send('Queue has been cleared.')
-        else:
-            await ctx.send('Queue is empty.')
-
-    @commands.command(aliases=['qs'])
-    async def queue_save(self, ctx, name = ''):
-        with open(PATH + '/queues.json', 'r', encoding='utf-8') as file:
-            queues = json.load(file)
+        embed = discord.Embed(title=f'Connected to {channel}')
+        await ctx.send(embed=embed, delete_after=20)
         
-        if name != '' and name not in queues:
-            queues[name] = [song['title'] for song in self.music_queue]
-            await ctx.send('Queue was saved.')
-        else:
-            await ctx.send('Failed to save queue.')
-        
-        with open(PATH + '/queues.json', 'w', encoding='utf-8') as file:
-            json.dump(queues, file)  
+    @commands.command()
+    async def play_(self, ctx, *query):
+        query = ' '.join(query)
 
-    @commands.command(aliases=['ql'])
-    async def queue_load(self, ctx, name = ''):
-        with open(PATH + '/queues.json', 'r', encoding='utf-8') as file:
-            queues = json.load(file)
-        
-        if name != '' and name in queues:
-            for song in queues[name]:
-                self.add_music(song)
-            await ctx.send('Queue has been loaded.')
-        else:
-            await ctx.send('Failed to load queue.')
+        vc = ctx.voice_client
 
+        if vc is None:
+            await ctx.invoke(self.connect_)
+
+        player = self.get_player(ctx)
+
+        if len(query) != 0:
+            player.queue += self.get_song(ctx, query)
+
+        player.play_music()
+   
     @commands.Cog.listener() #activates when this cog in ready state
     async def on_ready(self):
         print('Music cog is active.')
